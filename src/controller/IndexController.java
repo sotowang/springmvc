d3dfa7e18dc1641005e83d6236eea2d0f9a682c6
sotowang <sotowang@qq.com>
2018-07-12 14:00:36 +0800

    使用注解

--- src/controller/IndexController.java
+++ src/controller/IndexController.java
@@ -1,12 +1,14 @@
 package controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-public class IndexController implements Controller {
+@Controller
+public class IndexController {
+    @RequestMapping("/index")
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) {
         ModelAndView mav = new ModelAndView("index");
         mav.addObject("message", "hello Spring MVC");
