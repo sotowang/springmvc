f1e945d34e937304c5e855913fc8d898b4cffef4
sotowang <sotowang@qq.com>
2018-07-12 14:19:25 +0800

    session

--- src/controller/IndexController.java
+++ src/controller/IndexController.java
@@ -6,6 +6,8 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class IndexController {
     @RequestMapping("/index")
@@ -19,5 +21,17 @@ public class IndexController {
         ModelAndView mav = new ModelAndView("redirect:/index");
         return mav;
     }
+
+    @RequestMapping("/check")
+    public ModelAndView check(HttpSession session) {
+        Integer i = (Integer) session.getAttribute("count");
+        if (i == null)
+            i = 0;
+        i++;
+        session.setAttribute("count", i);
+        ModelAndView mav = new ModelAndView("check");
+        return mav;
+    }
+
 
 }
